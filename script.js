e82beee618fa0a0ebe37ddeadd072c4c3de5e16c
Tushar Pachouri <tusharpachouri001@gmail.com>
2025-05-13 02:34:16 +0530

    Refactor navigation and layout; add responsive design and new features for incident management

---
+++ script.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector(".hamburger");
+const menubar = document.querySelector(".menubar");
+
+hamburger.addEventListener("click", () => {
+  menubar.classList.toggle("active");
+  hamburger.classList.toggle("hamburger-active");
+});
